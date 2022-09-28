478faa7f4d0be299985b34d35a5f037b6bfc1a4a
JamesHe2001 <63664487+JamesHe2001@users.noreply.github.com>
2022-09-28 17:57:15 +0800

    Update views.py

--- Webapp/ridgefield/accounts/views.py
+++ Webapp/ridgefield/accounts/views.py
@@ -1,21 +1,29 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from accounts.forms import AccessRequestForm
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your views here.
-def register(request):
-    if request.method == 'POST':
-        form = AccessRequestForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('submitted')
-    else:
-        form = AccessRequestForm()
-    return render(request, 'registration/register.html', {'form': form})
+# Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
 
-def submitted(request):
-    return render(request, 'registration/request_sent.html')
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = []
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'role': self.role,
+            'is_active': self.is_active
+        }
