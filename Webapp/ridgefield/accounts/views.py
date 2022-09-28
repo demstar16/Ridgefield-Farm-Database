9cfaa0e7fccf524aedebc772ed57f08bf1fe4ac2
JamesHe2001 <63664487+JamesHe2001@users.noreply.github.com>
2022-09-28 17:58:28 +0800

    Update views.py

--- Webapp/ridgefield/accounts/views.py
+++ Webapp/ridgefield/accounts/views.py
@@ -1,29 +1,62 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class Role(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from accounts.forms import AccessRequestForm
+from django.http.response import HttpResponseRedirect
+from django.views.generic.base import View
+# Create your views here.
+from accounts.models import User
+from django.urls import reverse
 
 
-class User(AbstractUser):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(unique=True)
-    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
-    bio = models.TextField(null=True, blank=True)
-    is_active = models.BooleanField(default=False)
+def register(request):
+    if request.method == 'POST':
+        form = AccessRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            return redirect('submitted')
+    else:
+        form = AccessRequestForm()
+    return render(request, 'registration/register.html', {'form': form})
 
-    REQUIRED_FIELDS = []
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-            'role': self.role,
-            'is_active': self.is_active
-        }
+def submitted(request):
+    return render(request, 'registration/request_sent.html')
+
+
+# user list
+def audit_list(request):
+    users = User.objects.all()
+    user_list = []
+    for user in users:
+        user_list.append(user.to_dict())
+    return render(request, 'registration/audit_account.html', {'users': users})
+
+
+# Aduit passed
+def audit_activate(request, pk):
+    user = User.objects.get(id=int(pk))
+    user.is_active = 1
+    user.save()
+    users = User.objects.all()
+    user_list = []
+    for user in users:
+        user_list.append(user.to_dict())
+    return render(request, 'registration/audit_account.html', {'users': users})
+
+
+# Audit not passed
+def audit_freeze(request, pk):
+    user = User.objects.get(id=int(pk))
+    user.is_active = 0
+    user.save()
+    users = User.objects.all()
+    user_list = []
+    for user in users:
+        user_list.append(user.to_dict())
+    return render(request, 'registration/audit_account.html', {'users': users})
+
+
