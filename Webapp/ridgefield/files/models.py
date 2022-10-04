3f9a9cb12f626f72884ab8af474764deb7713899
ptinapple <ptinapple@hotmail.com>
2022-10-04 10:23:57 +0800

    +index page refresh

--- Webapp/ridgefield/files/models.py
+++ Webapp/ridgefield/files/models.py
@@ -6,6 +6,9 @@ from accounts.models import User
 class Paddock(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
