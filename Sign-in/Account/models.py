02b29318808bd1a4222286ab84cbe2c689b24b50
JamesHe2001 <63664487+JamesHe2001@users.noreply.github.com>
2022-10-07 16:23:07 +0800

    Add files via upload

---
+++ Sign-in/Account/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+    create_time = models.DateTimeField(auto_now_add=True,null=True, blank=True, verbose_name='Registration time')
+    information = models.CharField(max_length=255, null=True, blank=True, verbose_name='Information',
+                                   help_text=('Name, direction of use, purpose of use'),)
+
+    REQUIRED_FIELDS = ['email']
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'role': self.role,
+            'is_active': self.is_active
+        }
